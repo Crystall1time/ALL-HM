c9fb9c4d8aafb8839d92be4e46628d788b403349
Crystall1time <48089698+Crystall1time@users.noreply.github.com>
2019-03-18 23:16:01 +0300

    Add files via upload

---
+++ ALL HM/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstMassive = new int[10];
+            Random k = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                firstMassive[i] = k.Next(0, 10);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("\nFirst Massive is :");
+                Console.WriteLine(firstMassive[i]);
+            }
+
+            int[] secondMassive = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                secondMassive[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("\nSecond Massive is :");
+                Console.WriteLine(secondMassive[i]);
+
+            }
+
+            int[] thirdMassive = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                thirdMassive[i] = (firstMassive[i] + secondMassive[i]);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("\nThird Massive is: ");
+                Console.WriteLine(thirdMassive[i]);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
---
+++ ALL HM/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write($"Input {i + 1} array value: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write("\nYour array is:  ");
+                Console.Write(array[i]);
+            }
+            Console.Write("\nInput additional number: ");
+            int adNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nInput its position: ");
+            int numPosition = Convert.ToInt32(Console.ReadLine());
+            for (int i = 4; i >= numPosition; i--)
+                array[i] = array[i - 1];
+            array[numPosition - 1] = adNumber;
+            Console.Write("\nFinal array is :");
+            foreach (int p in array) Console.Write("{0,3}", p);
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+
+            int tempNum;
+            for (int i = 0; i < (5 / 2); i++)
+            {
+                tempNum = array[i];
+                array[i] = array[5 - i - 1];
+                array[5 - i - 1] = tempNum;
+            }
+
+            Console.Write("Revers array is :");
+            foreach (int p in array) Console.Write("{0,3}", p);
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
