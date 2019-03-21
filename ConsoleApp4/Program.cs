2feb9633d3c64ef0a085358d9e789ea426eb9178
Crystall1time <48089698+Crystall1time@users.noreply.github.com>
2019-03-21 19:54:16 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nullArray = new int[0];
+            Console.Write(nullArray.Length);
+            Console.ReadKey();
+        }
+    }
+}
